a0c6a4f7de08342f65e2286a0a82fc7028f60f40
j costakis <jcostakis@hotmail.com>
2023-07-01 00:18:14 -0400

    Attempt to create GUI using pygame

--- text_rpg.py
+++ text_rpg.py
@@ -2,8 +2,30 @@
 text_rpg
 
 This module will be the entry point for the game and for now will contain the main loop.
+
+TODO: add typing
 """
 import pygame
+
+class TextInput:
+    """The UI element where the player will input their commands
+
+    Will handle keypresses when active.
+    """
+    def __init__(self):
+        """Active is false and contents are empty upon creation.
+        """
+        self.active = False
+        self.contents = ""
+
+    def handle_keypress(self, keypress):
+        """Handles keypress by the player if it is currently active
+        
+        keypress: a string representing the player's keypress 
+        """
+        self.contents += keypress
+        print(self.contents)
+        pass
 
 if __name__ == "__main__":
     # Set up 
@@ -12,6 +34,9 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
     running = True
 
+    # Create UI elements
+    command_line = TextInput()
+
     # Game loop
     while running:
         # Process events
@@ -19,6 +44,9 @@ if __name__ == "__main__":
             match event.type:
                 case pygame.QUIT:
                     running = False
+                case pygame.TEXTINPUT:
+                    print("success")
+                    command_line.handle_keypress(event.unicode)
 
         # Update
         # Do i get pressed keys here or in processing events?
