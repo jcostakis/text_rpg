5e6bc0dd5ff902845ec3561a6ef905f46a73f68a
j costakis <jcostakis@hotmail.com>
2023-09-13 00:47:01 -0400

    Add pygame gui, test it, get rid of unneeded code

--- text_rpg.py
+++ text_rpg.py
@@ -6,57 +6,46 @@ This module will be the entry point for the game and for now will contain the ma
 TODO: add typing
 """
 import pygame
+import pygame_gui
 
-class TextInput:
-    """The UI element where the player will input their commands
+def main():
+    """Contains the main game loop"""
+    screen_resolution = (1280, 720)
 
-    Will handle keypresses when active.
-    """
-    def __init__(self):
-        """Active is false and contents are empty upon creation.
-        """
-        self.active = False
-        self.contents = ""
-
-    def handle_keypress(self, keypress):
-        """Handles keypress by the player if it is currently active
-        
-        keypress: a string representing the player's keypress 
-        """
-        self.contents += keypress
-        print(self.contents)
-        pass
-
-if __name__ == "__main__":
     # Set up 
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode(screen_resolution)
     clock = pygame.time.Clock()
     running = True
 
-    # Create UI elements
-    command_line = TextInput()
+    manager = pygame_gui.UIManager(screen_resolution)
+
+    hello_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((350, 275), (100, 50)), text="Say hello", manager=manager)
 
     # Game loop
     while running:
+        time_delta = clock.tick(60)/1000.00
+
         # Process events
         for event in pygame.event.get():
             match event.type:
                 case pygame.QUIT:
                     running = False
-                case pygame.TEXTINPUT:
-                    print("success")
-                    command_line.handle_keypress(event.unicode)
+            
+            manager.process_events(event)
 
         # Update
         # Do i get pressed keys here or in processing events?
+        manager.update(time_delta)
 
         # Render 
         # Wipe away previous frame
         screen.fill("purple") 
+        manager.draw_ui(screen)
         pygame.display.flip()
-
-        clock.tick(60) # Limit fps
 
     # Quit game
     pygame.quit()
+
+if __name__ == "__main__":
+    main()
