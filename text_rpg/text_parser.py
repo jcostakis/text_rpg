b315444cb5cef761eee618ce2103576e94dd2151
j costakis <jcostakis@hotmail.com>
2024-08-30 02:00:25 -0400

    Improve documentation and readability of parser

--- text_rpg/text_parser.py
+++ text_rpg/text_parser.py
@@ -1,31 +1,52 @@
+"""The module that converts user input text into commands and relevant events.
+
+This module is responsible for validating and determining what kind of request
+the user is attempting. It will report an error if an invalid input is detected
+and otherwise produce an event that will communicate the user's request to the
+rest of the game.
+"""
+
 import enum
 
 from event_manager import EventManager
 from events.io_events import ClearOutputRequest
 from events.system_events import QuitEvent
 
+
 class Tokens(enum.Enum):
+    """Tokens representing possible inputs by the user."""
+
     CLEAR = 1
     ERROR = 2
     QUIT = 3
 
 
+# Maps between keywords and valid tokens
+TOKEN_MAP = {
+    "clear": Tokens.CLEAR,
+    "quit": Tokens.QUIT,
+    "exit": Tokens.QUIT,
+}
+
+
 class TextParser:
+    """Converts user text input into events that other modules can respond to."""
 
     def __init__(self, event_manager: EventManager):
         self.event_manager: EventManager = event_manager
 
     def handle_input(self, input_string: str) -> None:
+        """ "Generates events to communicate user requests to system."""
         tokens = self.tokenize(input_string)
-        if len(tokens) == 1: 
+        if len(tokens) == 1:
             match tokens[0]:
                 case Tokens.CLEAR:
                     self.event_manager.queue_event(ClearOutputRequest())
                 case Tokens.QUIT:
                     self.event_manager.queue_event(QuitEvent())
-            
 
     def tokenize(self, string: str) -> list[Tokens]:
+        """Converts input str into a list of tokens."""
         tokens = []
 
         # Get rid of input prompt:
@@ -33,12 +54,7 @@ class TextParser:
 
         words = string.split()
         for word in words:
-            match word.lower():
-                case "clear":
-                    tokens.append(Tokens.CLEAR)
-                case "quit" | "exit":
-                    tokens.append(Tokens.QUIT)
-                case _:
-                    tokens.append(Tokens.ERROR)
+            token = TOKEN_MAP.get(word.lower(), Tokens.ERROR)
+            tokens.append(token)
 
         return tokens
